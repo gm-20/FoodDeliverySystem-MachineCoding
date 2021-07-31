632517c69b9ae237d3caf8848049a4936d2a6d0f
gm-20 <rock.gm.9755@gmail.com>
2021-07-31 15:01:51 +0530

    Added Total Bill

--- src/main/java/services/CustomerManager.java
+++ src/main/java/services/CustomerManager.java
@@ -26,11 +26,26 @@ public class CustomerManager {
         Restro selectedRestro = strategy.executeStrategy(itemList, restroList);
 
         System.out.println("Order created from Restro ID: " + selectedRestro.getResId());
+
+
+        //Total Order
+
+        int orderTotal = 0;
+
+        for (Item item : itemList) {
+            if (selectedRestro.getItems().containsKey(item.getName())) {
+                orderTotal += selectedRestro.getItems().get(item.getName()).getPrice();
+            }
+        }
+
+        System.out.println("Bill Amount: " + orderTotal);
+
         Order order = Order.builder()
                 .orderId(UUID.randomUUID().toString())
                 .status(OrderLifeCycle.CREATED)
                 .itemList(itemList)
                 .restroId(selectedRestro.getResId())
+                .orderTotal(orderTotal)
                 .build();
 
         orderList.add(order);
